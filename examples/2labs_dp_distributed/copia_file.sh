0603c9b66df691dfb4ad12bd55f4596645144245
gisaba <gisaba1980@gmail.com>
2026-01-23 17:43:07 +0100

    copia file

---
+++ examples/2labs_dp_distributed/copia_file.sh
@@ -0,0 +1,4 @@
+scp barbato@192.168.1.219:/home/barbato/VillasTestBench/examples/2labs_dp_distributed/lab_a/app/logs/"dpsim*.log" ./lab_a/app/logs
+
+scp picone@192.168.1.220:/home/picone/VillasTestBench/examples/2labs_dp_distributed/lab_b/app/logs/"dpsim*.log" ./lab_b/app/logs
+
